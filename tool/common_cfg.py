b2caeecbb0402e8e3d93383b6f5e3f53e5d0ed28
Jhay Mark Nedic (Ned) <jnedic@hennypenny.com>
2026-07-21 19:25:08 +0800

    Add cfg for common variables

---
+++ tool/common_cfg.py
@@ -0,0 +1,21 @@
+
+class Common:
+    TOOL_NAME = "Henny Penny Tool"
+
+    class Color:
+        HP_RED   = "#C9302C"
+        HP_BLACK = "#222222"
+
+    class State:
+        RO       = "readonly"
+        NORM     = "normal"
+        DIS      = "disabled"
+
+    class Tag:
+        INF      = "info"
+        ERR      = "error"
+        SUC      = "success"
+        WARN     = "warning"
+        TX       = "tx"
+        RX       = "rx"
+        VER      = "ver"
